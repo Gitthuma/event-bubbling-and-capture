3d59cb6a07f6eda3a9385517ffede1501c225417
Gitthuma <george.githuma@gmail.com>
2021-04-30 22:46:12 +0300

    Referenced the <button>, <div> and <video> and store them in variables

---
+++ code/main.js
@@ -0,0 +1,5 @@
+/*Reference the <button>, <div> and <video> and store them in variables*/
+
+const btn = document.querySelector('button');
+const videoBox = document.querySelector('div');
+const video = document.querySelector('video');
