c607807c0b2cdd4bd273eebe49dcce36bc351fbd
Gitthuma <george.githuma@gmail.com>
2021-04-30 22:54:24 +0300

    Created a function, that when the <button> is clicked, the video is displayed, by changing the class attribute on the <div> from hidden to showing

--- code/main.js
+++ code/main.js
@@ -3,3 +3,9 @@
 const btn = document.querySelector('button');
 const videoBox = document.querySelector('div');
 const video = document.querySelector('video');
+
+/*Create a function, that when the <button> is clicked, the video is displayed, by changing the class attribute on the <div> from hidden to showing (CSS contains these two classes, which position the box off the screen and on the screen, respectively):*/
+
+btn.onclick = function() {
+    videoBox.setAttribute('class', 'showing');
+}
